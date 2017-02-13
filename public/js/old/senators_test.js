458dc179bd7a174faecad6745d227a13b6995b11
chris <karp505@gmail.com>
2017-02-13 11:38:02 -0500

    added missing files

---
+++ public/js/old/senators_test.js
@@ -0,0 +1,22 @@
+var senators = {
+   "CA":
+      {
+         "Position": "Senator",
+         "First Name": "Lamar",
+         "Last Name": "Alexander",
+         "Party": "R",
+         "Opposed/Silent": "Silent",
+         "Statement": "",
+         "Statement Link": "",
+         "Reelection Year": 2020,
+         "Address": "455 Dirksen Senate Office Building",
+         "Phone Number": "202-224-4944",
+         "Email/Contact Form": "https://www.alexander.senate.gov/public/index.cfm/email",
+         "Twitter Link": "https://twitter.com/SenAlexander",
+         "Handle": "@SenAlexander",
+         "Press/Campaign Handle": "@LamarAlexander",
+         "Local Office Address": "Joel E. Soloman Federal Building, 900 Georgia Avenue, #260, Chattanooga, TN 37402",
+         "Local Office Number": "(423) 752-5337"
+      }
+}
+
