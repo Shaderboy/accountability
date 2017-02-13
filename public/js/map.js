6f63b213862a3b7dc752ec6025660a4490e35b3a
chris <karp505@gmail.com>
2017-02-13 11:37:52 -0500

    porting updates back from political-revolution.com

--- public/js/map.js
+++ public/js/map.js
@@ -1,37 +1,26 @@
-function tooltipHtml(n, d){
-	var elem = "<h4>"+n+"</h4><div class='row'>"+
-		"<div class='col-xs-6' id='tooltip-body-col'><div class='row'><h6>"+(d[0]["First Name"] + " " + d[0]["Last Name"])+"</h6></div><div class='row'><p>"+(d[0]["Stance"])+"</p></div><div class='row'><p>";
+  function senatorHtml(d){
+    var senBody = "<div class='col-xs-6 tooltip-body-col'><div class='row'><h6>"+(d["First Name"] + " " + d["Last Name"])+"</h6></div><div class='row '><p class='senator-status'>"+(d["Stance"])+"</p></div><div class='row'><p>";
+    senBody += d["Statement"];
+    senBody += "</p></div><div class='row'><p><a target='_blank' href = '"+ (d["Statement Link"]) + "'>Source</a></p></div><div class='row'><p><strong>Phone Number:</strong> "+(d["Local Office Number"])+"</p></div></div>";
+    return senBody;
+  }
 
-	var addQuotes = d[0]["Statement"].indexOf('"') == -1;
-	//if (addQuotes) elem += '"';
-	elem += d[0]["Statement"];
-	//if (addQuotes) elem += '"';
+  function tooltipHtml(n, d){
+	var html = "<h4>"+n+"</h4><div class='row'>" + senatorHtml(d[0]) + senatorHtml(d[1]) +"</div></div>";
+    return html;
+  }
 
-	elem += "</p></div><div class='row'><a href = '"+ (d[0]["Statement Link"]) + "'><p>"+(d[0]["Statement Link"])+"</a></p></div><div class='row'><p>"+(d[0]["Local Office Number"])+"</p></div></div>"+
-		"<div class='col-xs-6' id='tooltip-body-col'><div class='row'><h6>"+(d[1]["First Name"] + " " + d[1]["Last Name"])+"</h6></div><div class='row'><p>"+(d[1]["Stance"])+"</p></div><div class='row'><p>"
+  d3.select("div#map-container")
+    .append("div")
+    .classed("svg-container", true) //container class to make it responsive
+    .append("svg")
+    //responsive SVG needs these 2 attributes and no width and height attr
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    //.attr("viewBox", "0 0 1120 700")
+    .attr("viewBox", "0 0 960 600")
+    //.attr("viewBox", "0 0 " + window.innerWidth + " " + window.innerWidth * .625)
+    .attr("id", "statesvg")
+    //class to make it responsive
+    .classed("svg-content-responsive", true); 
 
-	addQuotes = d[1]["Statement"].indexOf('"') == -1;
-	//if (addQuotes) elem += '"';
-	elem += d[1]["Statement"];
-	//if (addQuotes) elem += '"';
-
-	elem += "</p></div><div class='row'><a href = '"+ (d[1]["Statement Link"]) + "'><p>"+(d[1]["Statement Link"])+"</a></p></div><div class='row'><p>"+(d[1]["Local Office Number"])+"</p></div></div>"+
-		"</div></div>";
-
-	return elem;
-}
-
-d3.select("div#map-container")
-   .append("div")
-   .classed("svg-container", true) //container class to make it responsive
-   .append("svg")
-   //responsive SVG needs these 2 attributes and no width and height attr
-   .attr("preserveAspectRatio", "xMinYMin meet")
-   //.attr("viewBox", "0 0 1120 700")
-   .attr("viewBox", "0 0 960 600")
-   //.attr("viewBox", "0 0 " + window.innerWidth + " " + window.innerWidth * .625)
-   .attr("id", "statesvg")
-   //class to make it responsive
-   .classed("svg-content-responsive", true); 
-
-uStates.draw("#statesvg", senators, tooltipHtml);
+  uStates.draw("#statesvg", senators, tooltipHtml, $);
